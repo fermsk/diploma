555194e1068ccd11e1a5837dc93f2fdeec860729
Ivan <zhelezniivan@mail.ru>
2023-09-22 20:59:20 +0300

    change all

--- terraform-deprecated/main.tf
+++ terraform-deprecated/main.tf
@@ -120,7 +120,7 @@ resource "yandex_vpc_subnet" "subnet-1" {
 # }
 
 output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address > host
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
 }
 
 # output "external_ip_address_vm_2" {
