ae28b30125e56b85848a6e702cec55d2a820cffb
Ivan <zhelezniivan@mail.ru>
2023-09-22 20:33:14 +0300

    change all

--- terraform-deprecated/main.tf
+++ terraform-deprecated/main.tf
@@ -82,16 +82,16 @@ resource "yandex_compute_instance" "vm-1" {
 #   }
 # }
 #
-# resource "yandex_vpc_network" "network-1" {
-#   name = "network1"
-# }
-#
-# resource "yandex_vpc_subnet" "subnet-1" {
-#   name           = "subnet1"
-#   zone           = "ru-central1-a"
-#   network_id     = yandex_vpc_network.network-1.id
-#   v4_cidr_blocks = ["10.128.0.0/24"]
-# }
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["10.127.0.0/24"]
+}
 #
 # resource "yandex_vpc_subnet" "subnet-2" {
 #   name           = "subnet2"
