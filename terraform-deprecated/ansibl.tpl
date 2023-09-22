359a2839fe67cff1590654236db8d253c94e8259
Ivan <zhelezniivan@mail.ru>
2023-09-22 23:17:22 +0300

    change all

--- terraform-deprecated/ansibl.tpl
+++
@@ -1,8 +0,0 @@
-[master]
-${master_ip}
-
-[work-node1]
-${node1_ip}
-
-[work-node2]
-${node2_ip}
--- terraform-deprecated/outputs.tf
+++ terraform-deprecated/outputs.tf
@@ -1,3 +1,5 @@
 output "master" {
   value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
 }
+
+
