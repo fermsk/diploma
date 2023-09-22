e72031bfc0cc048f65620bc2ba6a54ee5fd975c1
Ivan <zhelezniivan@mail.ru>
2023-09-23 01:00:10 +0300

    change all

--- terraform-deprecated/outputs.tf
+++ terraform-deprecated/outputs.tf
@@ -1,8 +1,7 @@
 output "master" {
-  value = <<EOF
-    [master]
-    ${yandex_compute_instance.vm-1.network_interface.0.nat_ip_address}
-    EOF
+  value =
+    yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+
 }
 
 
