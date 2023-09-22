f66032ccde3172b531c6baafbf4a79140dcaafea
Ivan <zhelezniivan@mail.ru>
2023-09-22 16:00:13 +0300

    checkpoint

--- terraform-deprecated/main.tf
+++ terraform-deprecated/main.tf
@@ -93,6 +93,20 @@ resource "yandex_vpc_subnet" "subnet-1" {
   v4_cidr_blocks = ["10.128.0.0/24"]
 }
 
+resource "yandex_vpc_subnet" "subnet-2" {
+  name           = "subnet2"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["10.130.0.0/24"]
+}
+
+resource "yandex_vpc_subnet" "subnet-3" {
+  name           = "subnet3"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["10.132.0.0/24"]
+}
+
 output "internal_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.ip_address
 }
